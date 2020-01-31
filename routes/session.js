45cfdb5f1b31e176ee5fafd51726de0876c317cd
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-31 21:50:43 +0300

    Make all routers on async/await

--- routes/session.js
+++ routes/session.js
@@ -15,36 +15,39 @@ const router = express.Router();
  * Create session
  * https://documenter.getpostman.com/view/9580525/SW7ey5Jy?version=latest#7fa8317f-b1d0-45b0-9478-3077f7fd7e38
  */
-router.post('/', validation('createSession'), (req, res, next) => {
-  const validationErrors = validationResult(req);
+router.post('/', validation('createSession'), async (req, res, next) => {
+  try {
+    let validationErrors = validationResult(req);
 
-  if (!validationErrors.isEmpty()) {
-    return errorHandler(validationErrors.errors, 400, res, next);
-  }
+    if (!validationErrors.isEmpty()) {
+      throw validationErrors.errors;
+    }
 
-  const { username, password } = req.body;
-  const where = { [Op.or]: [{ username }, { email: username.toLowerCase() }] };
+    let { username, password } = req.body;
+    let where = {
+      [Op.or]: [{ username }, { email: username.toLowerCase() }]
+    };
 
-  models.user
-    .findOne({
+    let foundUser = await models.user.findOne({
       where,
       attributes: ['id', 'email', 'password', 'username']
-    })
-    .then(user => {
-      if (!user || !user.isValidPassword(password)) {
-        return next(createError(404, res.__('incorrect-username-password')));
-      }
+    });
 
-      const userResponse = {
-        id: user.id,
-        email: user.email,
-        username: user.username
-      };
+    if (!foundUser || !foundUser.isValidPassword(password)) {
+      throw 'incorrect-username-password';
+    }
 
-      req.session.user = userResponse;
-      res.status(201).json(userResponse);
-    })
-    .catch(next);
+    const userResponse = {
+      id: foundUser.id,
+      email: foundUser.email,
+      username: foundUser.username
+    };
+
+    req.session.user = userResponse;
+    res.status(201).json(userResponse);
+  } catch (error) {
+    errorHandler(error, 400, res, next);
+  }
 });
 
 router.delete('/', auth, async (req, res, next) => {
--- routes/user.js
+++ routes/user.js
@@ -11,19 +11,26 @@ const router = express.Router();
  * Create an account
  * https://documenter.getpostman.com/view/9580525/SW7ey5Jy?version=latest#f93a6969-8d9d-409e-a583-e549645c8223
  */
-router.post('/', validation('createUser'), (req, res, next) => {
-  const validationErrors = validationResult(req);
+router.post('/', validation('createUser'), async (req, res, next) => {
+  try {
+    let validationErrors = validationResult(req);
 
-  if (!validationErrors.isEmpty()) {
-    return errorHandler(validationErrors.errors, 400, res, next);
+    if (!validationErrors.isEmpty()) {
+      throw validationErrors.errors;
+    }
+
+    const { email, password, username } = req.body;
+
+    let createdUser = await models.user.create({ email, password, username });
+
+    if (!createdUser) {
+      throw createdUser;
+    }
+
+    res.sendStatus(201);
+  } catch (error) {
+    errorHandler(error, 400, res, next);
   }
-
-  const { email, password, username } = req.body;
-
-  models.user
-    .create({ email, password, username })
-    .then(() => res.sendStatus(201))
-    .catch(error => errorHandler(error, 400, res, next));
 });
 
 /*
