0c8967a9291386fe87edfbad36af00fb2294f530
Dmitriy Minaev <minaevdup@gmail.com>
2020-02-08 17:13:39 +0300

    Fix error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,37 +1,37 @@
 const Sequelize = require('sequelize');
 
 function errorHandler(errorModel, statusCode, res, next) {
+  res.status(statusCode);
+
   /*
    * sequelize validator
    */
   if (errorModel instanceof Sequelize.ValidationError) {
-    const errors = {};
+    let errors = {};
 
     errorModel.errors.forEach(error => {
       errors[error.path] = res.__(error.message);
     });
 
-    return res.status(statusCode).send({ errors });
+    return res.send({ errors });
   }
 
   /*
    * express-validator
    */
   if (typeof errorModel === 'object') {
-    const errors = {};
+    let errors = {};
 
     errorModel.forEach(error => {
       errors[error.param] = res.__(error.msg);
     });
 
-    return res.status(statusCode).send({ errors });
+    return res.send({ errors });
   }
 
   if (typeof errorModel === 'string') {
-    return res.status(statusCode).send(res.__(errorModel));
+    return res.send({ errors: { message: res.__(errorModel) } });
   }
-
-  return next(errorModel);
 }
 
 module.exports = errorHandler;
